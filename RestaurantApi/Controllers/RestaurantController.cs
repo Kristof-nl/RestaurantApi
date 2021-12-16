4ff95b7f8696e2a3337d80372e416d40e202c6fc
Krzysztof <krzysztof.hermaszewski.bee@outlook.com>
2021-12-17 00:04:28 +0100

    Start with controllers

--- RestaurantApi/Controllers/RestaurantController.cs
+++ RestaurantApi/Controllers/RestaurantController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RestaurantApi.Entities;
+using RestaurantApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,25 +14,50 @@ namespace RestaurantApi.Controllers
     public class RestaurantController : ControllerBase
     {
         private readonly RestaurantDbContext _dbContext;
-        public RestaurantController(RestaurantDbContext dbContext)
+        private readonly IMapper _mapper;
+        public RestaurantController(RestaurantDbContext dbContext, IMapper mapper)
         {
             _dbContext = dbContext;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
         {
             var restaurants = _dbContext
                 .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
                 .ToList();
 
-            return Ok(restaurants);
+            var restaurantsDto = _mapper.Map<List<RestaurantDto>>(restaurants);
+
+            return Ok(restaurantsDto);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Restaurant> Get([FromRoute] int id)
+        public ActionResult<RestaurantDto> Get([FromRoute] int id)
         {
-            var 
+            var restaurant = _dbContext
+                .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == id);
+                
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+
+            var restaurantDto = _mapper.Map<RestaurantDto>(restaurant);
+
+            return Ok(restaurantDto);
+        }
+
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody]CreateRestaurantDto dto)
+        {
+
         }
 
     }
